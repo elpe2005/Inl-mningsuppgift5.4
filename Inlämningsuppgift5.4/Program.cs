c63142e34175ac1d0848e0859431d206de753503
Bob156590 <Admin@TEB12707>
2023-01-27 10:17:55 +0100

    Add project files.

---
+++ Inlämningsuppgift5.4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace uppgift54
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] namn = { "ödla", "Elliot", "Daniel", "Malin", "Tobias" };
+
+            Array.Sort (namn);
+
+            Console.WriteLine(namn[0]);
+        }
+    }
+}
